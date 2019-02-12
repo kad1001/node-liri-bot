7db04426fc4669417ab2d1c6d7c1353dae4bac1b
Kelly davis <kellyadavis7@gmail.com>
2019-02-12 18:37:46 -0500

    spotify api

--- keys.js
+++ keys.js
@@ -1,7 +1,12 @@
-console.log('this is loaded');
-
+// console.log('keys.js is loaded');
+console.log('COMMANDS:');
+console.log('concert-this');
+console.log('movie-this');
+console.log('do-what-it-says');
+console.log('spotify-this-song');
 
 exports.spotify = {
-  id: process.env.spotifyID,
-  secret: process.env.spotifySecret
-};
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
--- liri.js
+++ liri.js
@@ -5,6 +5,51 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 // access keys info
-var spotify = new spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 
-console.log(this);
+var spotify = new Spotify(keys.spotify);
+
+// node liri.js spotify-this-song '<song name here>'
+
+// take in command line arguments
+var inputStr = process.argv;
+
+var request = inputStr[2];
+
+
+
+// determines request
+ switch (request) {
+     case ("spotify-this-song"):
+
+     var songName = process.argv[3];
+
+    // If no song is provided then your program will default to 'under pressure' by queen
+
+    // get song info
+    spotify
+    // search songNamesp
+        .search ({ type: 'track', query: songName })
+        .then(function(response) {
+
+            // returns first item in response object
+            var first = response.tracks.items[0];
+            var artist = first.album.artists[0].name;
+            var spotifyName = first.name;
+            var preview = first.preview_url;
+            var album = first.album.name;
+            console.log("Artist: " + artist);
+            console.log("Track name: " + spotifyName);
+            console.log("Preview URL: " + preview);
+            console.log("Album: " + album);
+          })
+
+          .catch(function(err) {
+            console.log(err);
+          });
+
+  
+    
+    break;
+
+ }
