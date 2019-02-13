57088c1f1db4b9f2abb15cbc2a590cf81d3c718f
Kelly davis <kellyadavis7@gmail.com>
2019-02-12 21:28:03 -0500

    putting the fun in functional

--- keys.js
+++ keys.js
@@ -5,8 +5,15 @@ console.log('movie-this');
 console.log('do-what-it-says');
 console.log('spotify-this-song');
 
+
+// to pass into liri.js  -----
+// access keys in .env
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
   };
   
+  // OMDb key
+exports.movies = process.env.OMDB;
+
+exports.bands = process.env.band_id;
--- liri.js
+++ liri.js
@@ -4,52 +4,143 @@ require("dotenv").config();
 // import the keys.js file and store in a var
 var keys = require("./keys.js");
 
+var axios = require('axios');
+var moment = require('moment');
+
+var fs = require('fs');
 // access keys info
 var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify(keys.spotify);
 
-// node liri.js spotify-this-song '<song name here>'
+var movieKey = keys.movies;
+
+var thatid = keys.bands;
 
 // take in command line arguments
 var inputStr = process.argv;
-
 var request = inputStr[2];
+var getThis = inputStr[3];
 
+
+// global functions 
+function getSpotify() {
+    spotify
+    .search ({ type: 'track', query: getThis })
+    .then(function(response) {
+        // returns first item in response object
+        var first = response.tracks.items[0];
+        var artist = first.album.artists[0].name;
+        var spotifyName = first.name;
+        var preview = first.preview_url;
+        var album = first.album.name;
+        console.log("Artist: " + artist);
+        console.log("Track name: " + spotifyName);
+        console.log("Preview URL: " + preview);
+        console.log("Album: " + album);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function getOMDB() {
+  // baseURL + params
+        // get request 
+        axios.get('http://www.omdbapi.com/', {
+            params: {
+                t: getThis,
+                apikey: movieKey
+            }
+            })
+            .then(function(response) {
+                var r = response.data;
+                var title = r.Title;
+                var year = r.Year;
+                var lang = r.Language;
+                var plot = r.Plot;
+                var stars = r.Actors;
+                var ratings = r.Ratings;
+
+            // ratings
+                for (var x in ratings) {
+                    console.log("Ratings: ", ratings[x]);
+                }
+
+                console.log("Title: "+ title);
+                console.log("Year: " + year);
+                console.log("Language: " + lang);
+                console.log("Plot: " + plot);
+                console.log("Starring: " + stars);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+}
+
+function getShow() {
+
+    axios.get("https://rest.bandsintown.com/artists/" + getThis + "/events", {
+        params: {
+           app_id: thatid
+        }
+    })
+    
+    .then(function(response) {
+        // console.log(response.data);
+        // Name of the venue
+        console.log("Venue info: ", response.data[0].venue);
+        // Venue location
+        // Date of the Event (use moment to format this as "MM/DD/YYYY")
+        var datetime =  response.data[0].datetime;
+        var newDate = moment(datetime).format("MM/DD/YYYY");
+        console.log("Concert date: ", newDate);
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+}
 
 
 // determines request
  switch (request) {
      case ("spotify-this-song"):
+        getSpotify();
+        break;
 
-     var songName = process.argv[3];
-
-    // If no song is provided then your program will default to 'under pressure' by queen
-
-    // get song info
-    spotify
-    // search songNamesp
-        .search ({ type: 'track', query: songName })
-        .then(function(response) {
-
-            // returns first item in response object
-            var first = response.tracks.items[0];
-            var artist = first.album.artists[0].name;
-            var spotifyName = first.name;
-            var preview = first.preview_url;
-            var album = first.album.name;
-            console.log("Artist: " + artist);
-            console.log("Track name: " + spotifyName);
-            console.log("Preview URL: " + preview);
-            console.log("Album: " + album);
-          })
-
-          .catch(function(err) {
-            console.log(err);
-          });
-
-  
-    
+        // DON'T USE '< >' FOR TITLE
+    case("movie-this"):
+      getOMDB();
     break;
 
+    // bandsintown api
+    case("concert-this"):
+        getShow();
+    break;
+
+    case("do-what-it-says"):
+    // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+        fs.readFile('random.txt', 'utf8', function(error, data){
+            if (error) {
+                return console.log(error);
+            } else {
+                var newData = data.split(",");
+                request = newData[0];
+                getThis = newData[1];
+
+                if (request === 'spotify-this-song') {
+                    getSpotify();
+                } else if (request === 'concert-this') {
+                    getShow();
+                } else if (request === 'movie-this') {
+                    getOMDB();
+                } else {
+                    console.log("Try again!");
+                }
+            }
+        })
+        // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+        // Edit the text in random.txt to test out the feature for movie-this and concert-this.
+
+        break;
  }
